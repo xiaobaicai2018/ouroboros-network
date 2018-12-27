08341752fd5cc810a82d84092f66aab4dcb17cdb
Andreas Triantafyllos <andreas.triantafyllos@iohk.io>
2018-12-27 13:31:08 +0200

    Ease ghc.

--- ouroboros-consensus/src/Ouroboros/Benchmark/STM.hs
+++ ouroboros-consensus/src/Ouroboros/Benchmark/STM.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Ouroboros.Benchmark.STM
     (
       bracketObserve
@@ -7,10 +9,9 @@ import           Data.Text
 
 import           Ouroboros.Network.MonadClass
 
-import           Cardano.BM.Data.LogItem
 import           Cardano.BM.Data.SubTrace
 import           Cardano.BM.Observer.Monadic (observeClose, observeOpen)
-import           Cardano.BM.Trace (Trace, subTrace, typeofTrace)
+import           Cardano.BM.Trace (Trace, logNotice, subTrace, typeofTrace)
 
 bracketObserve :: (MonadSTM n, MonadSay n) => Trace IO -> Text -> Tr n t -> n t
 bracketObserve logTrace0 name action = do
@@ -22,8 +23,18 @@ bracketObserve logTrace0 name action = do
     bracketObserve' NoTrace _ act =
         atomically act
     bracketObserve' subtrace logTrace act = do
-        countersid <- liftIOaction $ observeOpen subtrace logTrace
-        -- run action, returns result only
+        mCountersid <- liftIOaction $ observeOpen subtrace logTrace
+        -- run action; if an exception is caught will be logged and rethrown.
         t <- atomically act
-        liftIOaction $ observeClose subtrace logTrace countersid []
+
+        case mCountersid of
+            Left openException ->
+                -- since observeOpen faced an exception there is no reason to call observeClose
+                -- however the result of the action is returned
+                 liftIOaction $ logNotice logTrace ("ObserveOpen: " <> pack (show openException))
+            Right countersid -> do
+                    res <-  liftIOaction $ observeClose subtrace logTrace countersid []
+                    case res of
+                        Left ex -> liftIOaction $ logNotice logTrace ("ObserveClose: " <> pack (show ex))
+                        _ -> pure ()
         pure t
