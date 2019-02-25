01c7fd1fb22049650d6486274f2909ccf761ec59
Marcin Szamotulski <profunctor@pm.me>
2019-02-25 22:59:41 +0100

    Use putStr in MonadSay IO instance
    MonadaSay should be removed in favour of iohk-monitoring-framework.

--- io-sim-classes/src/Control/Monad/Class/MonadSay.hs
+++ io-sim-classes/src/Control/Monad/Class/MonadSay.hs
@@ -1,12 +1,17 @@
-module Control.Monad.Class.MonadSay where
+module Control.Monad.Class.MonadSay
+  ( MonadSay (..)
+  ) where
+
+import qualified Data.ByteString.Char8 as BSC
 
 import           Control.Monad.State
 
+-- TODO: remove MonadSay, in favour of iohk-monitoring-framework
 class Monad m => MonadSay m where
   say :: String -> m ()
 
 instance MonadSay IO where
-  say = print
+  say = BSC.putStr . BSC.pack . show
 
 instance MonadSay m => MonadSay (StateT s m) where
   say = lift . say
